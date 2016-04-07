01f67ca765e709c0cf50e0d480e4b8a5425f2464
HamzaAbdulilah <hamza.abdulilah@gmail.com>
2016-04-07 14:15:42 +0200

    Expression och klasserna färdiga

--- src/instruction/Expression.java
+++ src/instruction/Expression.java
@@ -6,17 +6,17 @@ import data.Memory;
 
 public abstract class Expression implements Instruction {
 	
-	protected Data data1, data2;
+	protected Data d1, d2;
 	protected Address address;
 	
-	public Expression(Data d1, Data d2, Address a){
-		this.data1 = d1;
-		this.data2 = d2;
-		this.address = a;
+	public Expression(Data d1, Data d2, Address address){
+		this.d1 = d1;
+		this.d2 = d2;
+		this.address = address;
 		
 	}
 	
-	protected abstract void operation( Data data1 , Data data2, Address adress1);
+	protected abstract void operation(Memory m, Data data1 , Data data2, Address adress1);
 	
 	public int execute(Memory m, int i){
 		return i;
